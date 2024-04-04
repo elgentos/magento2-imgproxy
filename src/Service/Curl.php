56efe38c1006a9d57cd500e143c41918a3ce2f89
Jeroen Boersma <jeroen@elgentos.nl>
2024-04-04 12:11:20 +0200

    Removed docblock in favor of Rosanne

--- src/Service/Curl.php
+++ src/Service/Curl.php
@@ -6,20 +6,14 @@ namespace Elgentos\Imgproxy\Service;
 
 class Curl extends \Magento\Framework\HTTP\Client\Curl
 {
-    /**
-     * @param       $uri
-     * @param array $params
-     *
-     * @return void
-     */
-    public function head($uri, array $params = [])
+    public function head(string $uri, array $params = []): void
     {
         $this->setOptions(
             [
-            CURLOPT_URL => $uri,
-            CURLOPT_RETURNTRANSFER => true,
-            //CURLOPT_CUSTOMREQUEST => 'HEAD',
-            //CURLOPT_NOBODY => true
+                CURLOPT_URL => $uri,
+                CURLOPT_RETURNTRANSFER => true,
+                //CURLOPT_CUSTOMREQUEST => 'HEAD',
+                //CURLOPT_NOBODY => true
             ]
         );
         $this->makeRequest('GET', $uri, $params);
